98908de9e63708b08008ad2034db6fe0f605b192
brilliancy <qtipju@gmail.com>
2019-10-19 21:03:06 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,70 @@
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+hs.hotkey.bind(hyper, "0", function()
+  hs.reload()
+end)
+
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "j"},
+  right = {hyper, "l"},
+  down = {hyper, "k"},
+  left = {hyper, "h"},
+  fullscreen = {hyper, "i"}
+})
+
+
+function findOrLaunch(a)
+	local app = hs.application.find(a)
+	if not app then
+		hs.application.launchOrFocus(a)
+	end
+	return hs.application.find(a)
+end
+
+hs.fnutils.each({  
+	{ key = "a", func = function() 
+      myTerm = findOrLaunch("iterm")
+      myTerm:selectMenuItem({"Shell","New Window"}) 
+      end
+  },
+  { key = "q", func = function()
+      ffox = hs.appfinder.appFromName("Firefox")
+      ffox:selectMenuItem({"File","New Window"})
+      ffox:activate()
+      end
+  },
+  { key = "z", func = function()
+      finder = hs.appfinder.appFromName("Finder")
+      finder:selectMenuItem({"File","New Finder Window"})
+      finder:activate()
+      end
+  }
+}, function(object) hs.hotkey.bind(hyper, object.key, object.func) end)
+
+
+
+
+hs.hotkey.bind(hyper, "o", function()
+  local win = hs.window.focusedWindow();
+  if not win then return end
+  
+win:moveToScreen(win:screen():next())
+end)
+
+local applicationHotkeys = {
+  --a = 'Firefox',
+  --d = 'SublimeText',
+--s = 'Spotify',
+}
+
+for key, app in pairs(applicationHotkeys) do
+  hs.hotkey.bind(hyper, key, function()
+   hs.application.launchOrFocus(app)
+  end)
+end
+
+hs.timer.doAt("0:00","1h", function() hs.alert("Ding Dong") end)
